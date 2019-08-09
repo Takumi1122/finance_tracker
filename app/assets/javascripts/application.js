f73bdadfec3df8abbd1212d1072376dce697d187
Takumi1122 <takumi221b4869@gmail.com>
2019-08-10 08:10:04 +0900

    complete finance tracker app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,14 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function () {
-  $('#stock-lookup-form').on('ajax:complete', function (event, data, status) {
-    $('#results').html(data.responseText)
-  })
-})
+// $(document).ready(function () {
+//   $('#stock-lookup-form').on('ajax:complete', function (event, data, status) {
+//     $('#results').html(data.responseText)
+//   })
+// })
+
+// $(document).ready(function () {
+//   $('#friend-lookup-form').on('ajax:complete', function (event, data, status) {
+//     $('#results').html(data.responseText)
+//   })
+// })
