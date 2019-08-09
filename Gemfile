3b6bc75f98e9554e1f50c19f2cb1ba4ccd249fb8
Takumi1122 <takumi221b4869@gmail.com>
2019-08-09 09:35:11 +0900

    Add search api

--- Gemfile
+++ Gemfile
@@ -36,7 +36,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'devise'
 gem 'devise-bootstrap-views'
 gem 'bootstrap-sass'
-gem 'stock_quote' 
+gem 'stock_quote'
+gem 'iex-ruby-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+ 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+ 
+  def self.new_from_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'pk_305fca6aa0ed4ceb91521067c663d3d6')
+    looked_up_stock = client.quote(ticker_symbol)
+    new(name: looked_up_stock.company_name,
+        ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+  end
 end
