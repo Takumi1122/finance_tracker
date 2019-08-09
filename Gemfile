b8e5b863609e92bf8cc2056a9fc125bd585168e9
Takumi1122 <takumi221b4869@gmail.com>
2019-08-09 22:20:34 +0900

    Add search and ajax

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'devise-bootstrap-views'
 gem 'bootstrap-sass'
 gem 'stock_quote'
 gem 'iex-ruby-client'
+gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,17 @@
 class Stock < ApplicationRecord
- 
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
- 
+
   def self.new_from_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: 'pk_305fca6aa0ed4ceb91521067c663d3d6')
-    looked_up_stock = client.quote(ticker_symbol)
-    new(name: looked_up_stock.company_name,
-        ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    begin
+      looked_up_stock = client.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name,
+          ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
   end
 end
